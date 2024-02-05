188477df6c222338c2e4aea32880f38c94f3bf31
MiruGrigoras <miruna.grig@gmail.com>
2024-02-05 22:35:56 +0100

    Added basic routing

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 style="font-size: 48px;">Hello, World!</h1>
+        <NuxtLink to="/accommodations">Accommodations</NuxtLink>
+    <NuxtLink to="/plane-tickets">Plane Tickets</NuxtLink>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+}
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
---
+++ pages/plane-tickets.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <a-card style="width: 300px" v-for="card in cards" :key="card.id">
+            <a-card-meta :title="card.title" :description="card.description" />
+            <a-img :src="card.image" alt="Card Image" />
+        </a-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    title: "Card 1",
+                    description: "Description for Card 1",
+                    image: "https://example.com/card1.jpg",
+                },
+                {
+                    id: 2,
+                    title: "Card 2",
+                    description: "Description for Card 2",
+                    image: "https://example.com/card2.jpg",
+                },
+                {
+                    id: 3,
+                    title: "Card 3",
+                    description: "Description for Card 3",
+                    image: "https://example.com/card3.jpg",
+                },
+            ],
+        };
+    },
+};
+</script>
