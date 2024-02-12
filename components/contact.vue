e371060847b98948466dcfc2184bb0cc93e457ef
MiruGrigoras <miruna.grig@gmail.com>
2024-02-12 19:40:55 +0100

    feat: Add contact form

--- components/contact.vue
+++ components/contact.vue
@@ -1,43 +1,113 @@
 <template>
   <div>
     <a-button type="primary" @click="showModal">Conteaz&#259;-ne</a-button>
-    <a-modal v-model:open="open" title="Title" @ok="handleOk">
+    <a-modal v-model:open="open" title="Scrie-ne">
       <template #footer>
         <a-button key="back" @click="handleCancel">Return</a-button>
         <a-button
           key="submit"
           type="primary"
           :loading="loading"
-          @click="handleOk"
+          html-type="submit"
           >Submit</a-button
         >
       </template>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <a-form
+        :model="formState"
+        name="basic"
+        layout="horizontal"
+        :label-col="{ span: 4 }"
+        :wrapper-col="{ span: 20 }"
+        autocomplete="off"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+      >
+        <a-form-item
+          name="email"
+          label="E-mail"
+          :rules="[
+            {
+              type: 'email',
+              message: 'Adresa de e-mail nu are un format valid!',
+            },
+            { required: true, message: 'Te rog introdu adresa de e-mail!' },
+          ]"
+        >
+          <a-input
+            v-model:value="formState.email"
+            placeholder="Ex. nume@domeniu.com"
+          />
+        </a-form-item>
+        <a-form-item
+          name="title"
+          label="Titlu"
+          :rules="[
+            {
+              required: true,
+              message: 'Te rog introdu un titlu valid!',
+            },
+          ]"
+        >
+          <a-input
+            v-model:value="formState.title"
+            placeholder="Ex. Cerere de ofert&#259; pentru Londra"
+          />
+        </a-form-item>
+        <a-form-item
+          name="message"
+          label="Mesaj"
+          :rules="[
+            {
+              required: true,
+              message: 'Te rog introdu un mesaj valid!',
+            },
+          ]"
+        >
+          <a-textarea
+            v-model:value="formState.message"
+            :auto-size="{ minRows: 5, maxRows: 5 }"
+          />
+        </a-form-item>
+      </a-form>
     </a-modal>
   </div>
 </template>
-<script lang="ts" setup>
-import { ref } from "vue";
-const loading = ref<boolean>(false);
-const open = ref<boolean>(false);
-
-const showModal = () => {
-  open.value = true;
-};
-
-const handleOk = () => {
-  loading.value = true;
-  setTimeout(() => {
-    loading.value = false;
-    open.value = false;
-  }, 2000);
-};
-
-const handleCancel = () => {
-  open.value = false;
+<script lang="ts">
+interface FormState {
+  email: string;
+  title: string;
+  message: string;
+}
+export default {
+  data() {
+    return {
+      loading: false,
+      open: false,
+      formState: {
+        email: "",
+        title: "",
+        message: "",
+      } as FormState,
+    };
+  },
+  methods: {
+    showModal() {
+      this.open = true;
+    },
+    handleCancel() {
+      this.open = false;
+    },
+    onFinish(values: FormState) {
+      console.log("Success:", values);
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+        this.open = false;
+      }, 2000);
+    },
+    onFinishFailed(errorInfo: any) {
+      console.log("Failed:", errorInfo);
+    },
+  },
 };
 </script>
