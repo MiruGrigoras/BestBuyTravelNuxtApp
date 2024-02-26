8c5a93f6a42799f1030a312bbd376c93d74ebc9a
MiruGrigoras <miruna.grig@gmail.com>
2024-02-26 10:07:20 +0200

    Feat: Add address and company info in contact form

--- components/contact.vue
+++ components/contact.vue
@@ -1,72 +1,89 @@
 <template>
   <div>
-    <a-modal v-model:open="openDrawer" title="Scrie-ne">
+    <a-modal
+      v-model:open="openDrawer"
+      title="Contacteaz&#259;-ne"
+      class="contact-modal"
+    >
       <template #footer>
-        <a-button key="back" @click="handleCancel">Return</a-button>
+        <a-button key="back" @click="handleCancel">&#206;napoi</a-button>
         <a-button type="primary" :loading="loading" @click="handleSubmit"
-          >Submit</a-button
+          >Trimite</a-button
         >
       </template>
-      <a-form
-        ref="form"
-        :model="formState"
-        name="basic"
-        layout="horizontal"
-        :label-col="{ span: 4 }"
-        :wrapper-col="{ span: 20 }"
-        autocomplete="off"
-        @finishFailed="onFinishFailed"
-      >
-        <a-form-item
-          name="email"
-          label="E-mail"
-          :rules="[
-            {
-              type: 'email',
-              message: 'Adresa de e-mail nu are un format valid!',
-            },
-            {
-              required: true,
-              message: 'Te rog introdu-&#355;i adresa de e-mail!',
-            },
-          ]"
-        >
-          <a-input
-            v-model:value="formState.email"
-            placeholder="Ex. nume@domeniu.com"
-          />
-        </a-form-item>
-        <a-form-item
-          name="title"
-          label="Titlu"
-          :rules="[
-            {
-              required: true,
-              message: 'Te rog introdu un titlu valid!',
-            },
-          ]"
-        >
-          <a-input
-            v-model:value="formState.title"
-            placeholder="Ex. Cerere de ofert&#259; pentru Londra"
-          />
-        </a-form-item>
-        <a-form-item
-          name="message"
-          label="Mesaj"
-          :rules="[
-            {
-              required: true,
-              message: 'Te rog introdu un mesaj valid!',
-            },
-          ]"
-        >
-          <a-textarea
-            v-model:value="formState.message"
-            :auto-size="{ minRows: 5, maxRows: 5 }"
-          />
-        </a-form-item>
-      </a-form>
+      <a-row>
+        <a-col :span="8">
+          Agen&#355;ie de turism online <br />
+          Adresa: Intrarea T&#226;rgu Frumos nr. 7, sector 4 Bucure&#351;ti
+          <br />
+          Telefon: +40 726 919 690 <br />
+        </a-col>
+        <a-col :span="1" style="display: flex; justify-content: center">
+          <a-divider type="vertical" style="height: 100%" />
+        </a-col>
+        <a-col :span="15">
+          <a-form
+            ref="form"
+            :model="formState"
+            name="basic"
+            layout="horizontal"
+            :label-col="{ span: 4 }"
+            :wrapper-col="{ span: 20 }"
+            autocomplete="off"
+            @finishFailed="onFinishFailed"
+          >
+            <a-form-item
+              name="email"
+              label="E-mail"
+              :rules="[
+                {
+                  type: 'email',
+                  message: 'Adresa de e-mail nu are un format valid!',
+                },
+                {
+                  required: true,
+                  message: 'Te rog introdu-&#355;i adresa de e-mail!',
+                },
+              ]"
+            >
+              <a-input
+                v-model:value="formState.email"
+                placeholder="Ex. nume@domeniu.com"
+              />
+            </a-form-item>
+            <a-form-item
+              name="title"
+              label="Titlu"
+              :rules="[
+                {
+                  required: true,
+                  message: 'Te rog introdu un titlu valid!',
+                },
+              ]"
+            >
+              <a-input
+                v-model:value="formState.title"
+                placeholder="Ex. Cerere de ofert&#259; pentru Londra"
+              />
+            </a-form-item>
+            <a-form-item
+              name="message"
+              label="Mesaj"
+              :rules="[
+                {
+                  required: true,
+                  message: 'Te rog introdu un mesaj valid!',
+                },
+              ]"
+            >
+              <a-textarea
+                v-model:value="formState.message"
+                :auto-size="{ minRows: 5, maxRows: 5 }"
+              />
+            </a-form-item>
+          </a-form>
+        </a-col>
+      </a-row>
     </a-modal>
   </div>
 </template>
@@ -127,3 +144,9 @@ export default {
   },
 };
 </script>
+<style>
+.contact-modal {
+  height: fit-content !important;
+  width: 60% !important;
+}
+</style>
