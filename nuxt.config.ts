df79b915c9aa4703a7c151274051d8027fabd2ed
MiruGrigoras <miruna.grig@gmail.com>
2024-03-01 09:32:59 +0200

    feat: add base url in config v9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/BestBuyTravelNuxtApp/', 
   },
+  vite: {
+    base: '/BestBuyTravelNuxtApp/',
+  },
   devtools: { enabled: true },
   ssr: false,
   modules: [
